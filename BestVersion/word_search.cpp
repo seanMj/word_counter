f626935cefcc62b9e349cbef49e686f79e619ff4
seanMj <74737560+seanMj@users.noreply.github.com>
2020-11-23 16:21:15 +0000

    Update word_search.cpp

--- BestVersion/word_search.cpp
+++ BestVersion/word_search.cpp
@@ -17,28 +17,28 @@ inline void count_words(const std::string& word, const std::string& filename)
 		size_t word_cnt = 0;
 
 		std::vector<std::string> tempv;
-
-		while(file >> temp){
-			for(auto& i : temp){
-				if(isupper(i))
-					i = tolower(i);
-				if(isspace(i))
-					i = '\n';
-				if(ispunct(i))
-					i = ' ';
+		
+		while(file >> temp){//read from file, word by word
+			for(auto& i : temp){//read each character of each word
+				if(isupper(i))		//if
+					i = tolower(i);	//do this 
+				if(isspace(i))		//if
+					i = '\n';	//do this
+				if(ispunct(i))		//if
+					i = ' ';	//then this
 			}
-			tempv.push_back(temp);
+			tempv.push_back(temp);		//place current word into the vector
 		}
-		std::sort(tempv.begin(), tempv.end());
+		std::sort(tempv.begin(), tempv.end());	//after going through the file, and collection, sort the vector [word3,word2,word1] = [word1,word2,word3]
 
 		//str::size_type could equally be called "auto"
 		for(std::string::size_type i = 0; i != tempv.size(); ++i){
-			if(word == tempv[i]){
-				word_cnt += 1;
+			if(word == tempv[i]){	//if this is the word we are looking for
+				word_cnt += 1;	//add 1 to the word count
 			}
 		}
-		std::cout << word_cnt << std::endl;	
-		file.close();
+		std::cout << word_cnt << std::endl;	//print the results of the data collected
+		file.close();				//close the file, it does not need to be open anymore currently.
 }
 //this is the best version: simple + to the point = works
 //no fancy pointers, no fancy words just a program that does a task.
