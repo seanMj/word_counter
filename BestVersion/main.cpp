c67458f99ccedb7b1ae88555f4c75d783c24d021
seanMj <74737560+seanMj@users.noreply.github.com>
2020-11-23 16:25:40 +0000

    Update main.cpp

--- BestVersion/main.cpp
+++ BestVersion/main.cpp
@@ -1,14 +1,16 @@
 #include "word_search.cpp"
 #include <iostream>
 #include <string>
-//..
 
-//i should use regex to find the words... but im only looking for specific words.
-//..
+//..				what word			filename to look at
 inline void count_words(const std::string& word, const std::string& filename);
 
 int main(){
-	const std::string str = "gold";
-	const std::string text = "bible.txt";		
-	count_words(str, text);
+	const std::string str = "gold";		//word to look for
+	const std::string text = "bible.txt";	//file to look through
+	count_words(str, text);			//call the function with the two parameters "gold", and "bible.txt"
+						//where gold is the word we want to count the occurences, and the "bible.txt" 
+						//is the file which we wish to search through. :)
+	
+			//you can do this little buddy!
 }
